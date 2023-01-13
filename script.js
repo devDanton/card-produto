87fbbab4b03eca4fc0d5d1147bf9d1e247e85607
Danton da Rosa Abreu <77130559+devDanton@users.noreply.github.com>
2023-01-12 21:30:34 -0300

    Add gif

---
+++ script.js
@@ -0,0 +1,17 @@
+const img = document.getElementById('produto')
+const icon = document.getElementById('graus')
+const button = document.getElementById('change')
+
+let toggle = false
+
+button.addEventListener('click', function () {
+  toggle = !toggle
+
+  if (toggle) {
+    img.src = './assets/produto.gif'
+    icon.src = './assets/fechar.svg'
+  } else {
+    img.src = './assets/produto.png'
+    icon.src = './assets/360.svg'
+  }
+})
